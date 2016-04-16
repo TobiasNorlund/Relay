28abdd2f4a5fd5605a00e643fe5cdd8643ddb0e5
TobiasNorlund <tobias@norlund.se>
2016-04-16 11:26:07 +0200

    Added manula control

---
+++ manual.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('WebPage1.html', name=name)
+
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0')
